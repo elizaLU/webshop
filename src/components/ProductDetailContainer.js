b3fabb1980a1ac45037ca23c9338438ff6d27619
vhs2708 <veronicahvaalstigen@me.com>
2019-10-30 16:29:59 +0100

    installed route and link

---
+++ src/components/ProductDetailContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+class ProductDetailContainer {
+  render() {
+    return (
+      <div className="productDetails">
+        <p>jdhfdjhfd</p>
+      </div>
+    );
+  }
+}
+
+export default connect(null)(ProductDetailContainer);
