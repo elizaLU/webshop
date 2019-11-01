25efedeab800b134141e7bca274af27ae393f4c8
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2019-11-01 15:12:46 +0100

    add total price in Cart

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -2,19 +2,26 @@ import React from "react";
 import { Link } from 'react-router-dom'
 
 export default function CartItems(props) {
-  console.log("add product ", props.addProduct)
+  console.log("Hello product ", props.addProduct)
+  const totalPrice = props.addProduct
+    .reduce((acc, currVal) => { return acc + parseInt(currVal.price) }, 0)
+
+
   return (
     <div>
-      <h4>You have the following items to your cart:</h4>
+      <h4>You have {props.addProduct.length} items to your cart:</h4>
       <div>
         {props.addProduct.map(addProduct =>
-          <span key={addProduct.id}> 
+          <span key={addProduct.id}>
             <Link to={`/products/${addProduct.id}`}>{addProduct.name}</Link>
             <br />Price: {addProduct.price}
+            <br />
           </span>
         )}
       </div>
-
+      <p>Total price: {totalPrice}</p>
+      <br />
+      <button>CLEAR CART</button>
     </div>
   )
 }
